18bf0d25370f8f0bfaaa0cdfb41c436e8a0dc777
Agustin Pane <agustin.pane@gmail.com>
2018-12-17 12:13:27 -0300

    Added decrement, subtract, add handlers

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -36,6 +36,15 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store result</button>
+                <ul>
+                    {
+                        this.props.storedResults.map(result => (
+                            <li onClick={() => this.props.onDeleteResult(result.id)} key={result.id}>{result.value}</li>
+                        ))
+                    }
+                </ul>
             </div>
         );
     }
@@ -43,7 +52,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
@@ -54,6 +64,8 @@ const mapDispatchToProps = dispatch =>{
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: () => dispatch({type: 'ADD', value: 5}),
         onSubtractCounter: () => dispatch({type: 'SUBTRACT', value: 5}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultId: id}),
     }
 };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,35 +1,49 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 };
 
 const reducer = (state = initialState,action) =>{
     switch(action.type){
         case 'INCREMENT':{
             return {
-                ...initialState,
+                ...state,
                 counter: state.counter+1
             }
         }
         case 'DECREMENT':{
             return {
-                ...initialState,
+                ...state,
                 counter: state.counter-1
             }
         }
         case 'ADD':{
             return {
-                ...initialState,
+                ...state,
                 counter: state.counter+action.value
             }
         }
         case 'SUBTRACT':{
             return {
-                ...initialState,
+                ...state,
                 counter: state.counter-action.value
             }
         }
+        case 'STORE_RESULT':{
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter}) // Instead of push, concat returns a new array
+            }
+        }
+        case 'DELETE_RESULT':{
+            const updatedArray = state.results.filter(result => result.id !== action.resultId); // Instead of slice, concat filters generates a new array
+            return {
+                ...state,
+                results: updatedArray
+            }
+        }
         default:{
-            return initialState;
+            return state;
         }
     }
 };
