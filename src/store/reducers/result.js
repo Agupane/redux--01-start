e855a8ed0536885d99e33203b743d969fa62218a
Agustin Pane <agustin.pane@gmail.com>
2018-12-18 14:04:03 -0300

    Refactor

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,23 +1,26 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from "../utility";
 
 const initialState = {
     results: []
 };
 
+const deleteResult = (state,action)=>{
+    const updatedArray = state.results.filter(result => result.id !== action.resultId); // Instead of slice, concat filters generates a new array
+    return updateObject(state, {
+        results: updatedArray
+    });
+};
+
 const resultReducer = (state = initialState,action) =>{
     switch(action.type){
         case actionTypes.STORE_RESULT:{
-            return {
-                ...state,
+            return updateObject(state, {
                 results: state.results.concat({id: new Date(), value: action.value}) // Instead of push, concat returns a new array
-            }
+            });
         }
         case actionTypes.DELETE_RESULT:{
-            const updatedArray = state.results.filter(result => result.id !== action.resultId); // Instead of slice, concat filters generates a new array
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return deleteResult(state, action);
         }
         default:{
             return state;
